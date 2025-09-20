ed313c48c17ade0d9375345890e603585c2b9daf
icju <isicju@gmail.com>
2025-09-21 02:02:08 +0300

    draft

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.18.0/commons-lang3-3.18.0.jar
-
-javac -cp commons-lang3-3.18.0.jar Main.java
-
-java -cp ".:commons-lang3-3.18.0.jar" Main
-
--- script_jar.sh
+++ script_jar.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-wget https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.18.0/commons-lang3-3.18.0.jar
+javac SimpleServer.java
 
-javac -cp commons-lang3-3.18.0.jar Main.java
+echo "Main-Class: SimpleServer" > manifest.txt
 
-echo "Main-Class: Main" > manifest.txt
-echo "Class-Path: commons-lang3-3.18.0.jar" >> manifest.txt
+jar cfm SimpleServer.jar manifest.txt SimpleServer.class
 
-jar cfm Main.jar manifest.txt Main.class
+echo "Starting server..."
 
-java -jar Main.jar
+java -jar SimpleServer.jar
