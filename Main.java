cd73afa880c3731e705577ea3516129d4e0505ff
icju <isicju@gmail.com>
2025-09-20 22:29:47 +0300

    draft

--- Main.java
+++ Main.java
@@ -1,5 +1,32 @@
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Hello Java Enterprise course 2025!");
+
+        System.out.println("=== Apache Commons Lang3 Demo ===");
+
+        // Example 1: String manipulation
+        String text = "hello world";
+        System.out.println("Original: " + text);
+        System.out.println("Capitalized: " + StringUtils.capitalize(text));
+        System.out.println("Reversed: " + StringUtils.reverse(text));
+        System.out.println("Contains 'world': " + StringUtils.contains(text, "world"));
+
+        // Example 2: System information
+        System.out.println("\n=== System Information ===");
+        System.out.println("Java Home: " + SystemUtils.getJavaHome());
+        System.out.println("User Home: " + SystemUtils.getUserHome());
+        System.out.println("OS Name: " + SystemUtils.OS_NAME);
+        System.out.println("OS Version: " + SystemUtils.OS_VERSION);
+
+        // Example 3: Array utilities
+        System.out.println("\n=== Array Demo ===");
+        String[] fruits = {"apple", "banana", "cherry"};
+        System.out.println("Array: " + java.util.Arrays.toString(fruits));
+        System.out.println("Contains 'banana': " +
+                org.apache.commons.lang3.ArrayUtils.contains(fruits, "banana"));
     }
 }
--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-javac Main.java
-java Main
+wget https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.18.0/commons-lang3-3.18.0.jar
+
+javac -cp commons-lang3-3.18.0.jar Main.java
+
+java -cp ".:commons-lang3-3.18.0.jar" Main
