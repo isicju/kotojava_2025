f5d572bb680bbdcc5cc04f5d696d160738712042
icju <isicju@gmail.com>
2025-09-21 03:11:56 +0300

    build all classes

--- script_jar.sh
+++ script_jar.sh
@@ -4,7 +4,7 @@ javac SimpleServer.java
 
 echo "Main-Class: SimpleServer" > manifest.txt
 
-jar cfm SimpleServer.jar manifest.txt SimpleServer.class
+jar cfm SimpleServer.jar manifest.txt *.class
 
 echo "Starting server..."
 
